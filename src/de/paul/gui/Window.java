02ec76431fad5eb70c53fdf762c5feff53d063be
Paul <66725843+Paul-H18@users.noreply.github.com>
2021-12-02 08:47:07 +0100

    window begun

---
+++ src/de/paul/gui/Window.java
@@ -0,0 +1,14 @@
+package de.paul.gui;
+
+import javax.swing.*;
+
+public class Window {
+    private JFrame frame = new JFrame();
+    private JTextField ecryptField = new JTextField(), decryptField = new JTextField();
+    private JButton ecryptButton = new JButton(), decryptButton = new JButton();
+    private JLabel ecryptedText = new JLabel(), decryptedText = new JLabel();
+
+    public Window() {
+
+    }
+}
